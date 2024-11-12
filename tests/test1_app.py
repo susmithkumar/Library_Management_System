bb093216cf4b630e4493bac6657c78087ed8790f
BM-45 <bhanumallik.tallapragada@slu.edu>
2024-11-13 01:22:45 +0530

    adding github action file

--- tests/test1_app.py
+++ tests/test1_app.py
@@ -1,5 +1,10 @@
 import pytest
 from flask import session
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app, mysql
 import unittest
 
